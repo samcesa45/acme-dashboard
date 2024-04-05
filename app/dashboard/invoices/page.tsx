bfa17008cce0b7e5b006721f2415ac1960a023a1
Samuel <samcesa45@gmail.com>
2024-04-05 20:11:30 +0100

    initial checkins

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+const Page = () => {
+  return (
+    <div>Invoice Page</div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Page = () => {
+  return (
+    <div>Dashboard Page</div>
+  )
+}
+
+export default Page
