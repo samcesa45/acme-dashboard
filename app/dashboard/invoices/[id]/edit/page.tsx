6d544e8b5b1b25c51190775cad64ac59877ef718
Samuel <samcesa45@gmail.com>
2024-04-12 16:01:39 +0100

    added edit functionality

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { customers } from '@/app/lib/placeholder-data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import EditInvoiceForm from '@/app/ui/invoices/edit-form';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices', active: true },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+}
